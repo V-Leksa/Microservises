6f19ce4d07c7c5d7d4244869fa2a9f2f114ec53f
V-Leksa <157983588+V-Leksa@users.noreply.github.com>
2025-04-22 18:15:44 +0200

    Delete service4 directory

--- service4/src/reports/interfaces/reportData.interfaces.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from "mongoose";
-
-export interface ReportsData extends Document {
-    title: string,
-    reportInfo: string,
-    rating: number
-}
--- service4/src/reports/interfaces/reports.interfaces.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from "mongoose";
-import { ReportsData } from "./reportData.interfaces";
-
-export interface Reports extends Document {
-    readonly userId: number;
-    readonly goodId: number;
-    
-    readonly report: ReportsData
-}
